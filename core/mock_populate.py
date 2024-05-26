2c1fbcac4916abbee2f29001ae2886c8576fc027
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-26 20:20:07 +0200

    Working list endpoint

--- core/mock_populate.py
+++
@@ -1,9 +0,0 @@
-from core.models import Component
-
-def cleanup():
-  Component.objects.all().delete()
-
-def populate():
-  c = Component()
-  c.save()
-  assert len(Component.objecs.all())>0, "Population failed"
--- core/views.py
+++ core/views.py
@@ -3,12 +3,12 @@ from core.models import Component
 from rest_framework import serializers, generics
 from django.http import HttpResponse
 
-class ComponentSerializer(serializers.Serializer):
+class ComponentSerializer(serializers.ModelSerializer):
   class Meta:
     model = Component
-    fields = ('id', 'name')
+    fields = ('id', 'name', 'code', 'picture', 'datasheet_url', 'quantity', 'row', 'column', 'depth', 'protection')
 
 class ComponentListAPIView(generics.ListAPIView):
-  queryset = Component.objects.all()[:10]
+  queryset = Component.objects.all()
   serializer_class = ComponentSerializer
 
