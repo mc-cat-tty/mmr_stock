4b5b9497dde07c586a680e425ed55a8675d51cc5
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-26 10:56:46 +0200

    Rename DB, ignore it

--- mmr_stock/settings.py
+++ mmr_stock/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mmr_stock.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dev_db.sqlite3',
     }
 }
 
