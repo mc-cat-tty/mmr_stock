aebd3bed207f951029a59307adae362732871f3e
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-27 10:02:52 +0200

    Home template refinements

--- mmr_stock/settings.py
+++ mmr_stock/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w!*#=5t2m1w2wgtb*@u-3sv%!efj=%rnhjt+e8)1fj5#b^i_0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
