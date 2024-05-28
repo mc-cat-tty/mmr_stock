8248122b33791fd910b38cbb9905a9881a7f96f5
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-29 01:42:50 +0200

    Working on star create endpoint

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import favorites
 from django.urls import path
+from .views import favorites, StarCreateAPIView
 
 STATIC_URL = "/media/static"
 app_name = "analytics"
 
-urlpatterns = [
-  path("favorites/", favorites, name='favorites')
-]
+# urlpatterns = [
+#   path("favorites/", favorites, name="favorites"),
+#   path("favorites/add/", StarCreateAPIView.as_view()),
+# ]
+
+from .views import Star
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'favorites', StarCreateAPIView, r"favorites")
+urlpatterns = router.urls
--- analytics/views.py
+++ analytics/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-from core.models import Component
+from rest_framework import generics, serializers
+from .models import Star
 
 def favorites(request: HttpRequest) -> HttpResponse:
   context = {
     'pagename': 'Favorites',
-    'starred': Component.objects.all()
   }
 
   return render(request, template_name="favorites.html", context=context)
+
+class StarSerializer(serializers.Serializer):
+  class Meta:
+    model = Star
+    fields = ("user", "component")
+
+class StarCreateAPIView(generics.CreateAPIView):
+  queryset = Star.objects.all()
+  serializer_class = StarSerializer
