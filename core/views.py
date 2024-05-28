36db27f9099e687e75e8a9d24efd34ce2d16c570
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-28 07:46:31 +0200

    Add integer fields to modal

--- core/views.py
+++ core/views.py
@@ -4,24 +4,33 @@ from rest_framework import serializers, viewsets
 from django.http import HttpResponse, HttpRequest
 from .models import Component
 from django.shortcuts import render
-from django.db.models import CharField, TextField
+from django.db.models import CharField, TextField, IntegerField
 from operator import attrgetter
 
 def home(request: HttpRequest) -> HttpResponse:
-  component_text_fields = tuple(
-    filter(
-      lambda field: isinstance(field, CharField) or isinstance(field, TextField),
-      Component._meta.fields
-    )
+  component_text_fields = filter(
+    lambda field: isinstance(field, CharField) or isinstance(field, TextField),
+    Component._meta.fields
   )
+
+  component_numeric_fields = filter(
+    lambda field: isinstance(field, IntegerField) and field.name != 'id',
+    Component._meta.fields
+  )
+
   context = {
     'recommended': Component.objects.all()[:20],
     'components': Component.objects.all()[:9],
     'modal_textual_fields': map(
       attrgetter('name'),
       component_text_fields
+    ),
+    'modal_numeric_fields': map(
+      attrgetter('name'),
+      component_numeric_fields
     )
   }
+
   return render(request, template_name="home.html", context=context)
 
 class ComponentSerializer(serializers.ModelSerializer):
