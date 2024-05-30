b4ed915e60e2492263dd3df3f4a84995ca68977b
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-30 12:46:17 +0200

    Add Use relationship

--- core/views.py
+++ core/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.db.models import CharField, TextField, IntegerField
 from django.views.generic.list import ListView
+from django.utils import timezone
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
-from .models import Component, Profile
+from .models import *
+
 
 class HomeView(ListView):
   model = Component
@@ -87,12 +89,19 @@ class ComponentAPI(
 
   def partial_update(self, request: Request, *args, **kwargs):
     pk = kwargs.get('pk')
-    available_quantity = Component.objects.get(pk=pk).quantity
+    component = Component.objects.get(pk=pk)
+    available_quantity = component.quantity
     requested_quantity = int(request.data.get('quantity'))
     available_quantity -= requested_quantity
     
     if (available_quantity < 0): return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
     
     Component.objects.filter(pk=pk).update(quantity = available_quantity)
+    Use.objects.create(
+      profile=Profile.objects.get(user=self.request.user),
+      component=component,
+      date=timezone.now()
+    )
+
     state = {'quantity': requested_quantity}
     return Response(status=status.HTTP_200_OK, data=state)
