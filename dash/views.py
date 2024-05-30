979bb147c409f67d814cb0943205a9458d9786f8
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-30 15:46:24 +0200

    Move scss out of templates

--- dash/views.py
+++ dash/views.py
@@ -3,14 +3,17 @@ from django.views.generic.list import ListView
 from core.models import *
 
 class DashView(ListView):
-  model = User
+  model = Profile
   template_name="dashboard.html"
   
-  # def get_queryset(self):
-  #   queryset = super().get_queryset()
-  #   query = self.request.GET.get('query', '')
+  def get_queryset(self):
+    queryset = super().get_queryset()
+    query = self.request.GET.get('query', '')
 
-  #   self.form_data = {'query': query}
+    self.form_data = {'query': query}
 
-  #   return queryset.filter(username__icontains=query)
+    return queryset.filter(user__username__icontains=query)
 
+  def get_context_data(self, **kwargs):
+    ctx = super().get_context_data(**kwargs)
+    return ctx | self.form_data
