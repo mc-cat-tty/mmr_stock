36c6ead3331afdd8c934b41d6c48194b55f01a34
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-06-01 19:12:01 +0200

    Hook refactor

--- core/models.py
+++ core/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from os.path import join
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, *args, **kwargs):
   if created:
     Profile.objects.create(user=instance)
-
-post_save.connect(create_profile, sender=User)
 
 class Component(models.Model):
   """
