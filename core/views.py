ed22697876a46413698ff954e2b970b451c43b50
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-06-02 10:30:45 +0200

    Working profilepic update

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit  import UpdateView
 from django.urls import reverse_lazy
 from django.forms import inlineformset_factory
+from django import forms
 
 from .models import Profile, User
 
@@ -19,45 +20,17 @@ class LogoutView(LogoutView):
 class ProfileForm(ModelForm):
   class Meta:
     model = Profile
-    fields = ("propic",)
+    fields = ("propic","user")
 
 class UserForm(ModelForm):
   class Meta:
     model = User
     fields = ("username", "first_name", "last_name", "email")
 
-# ProfileFormSet = inlineformset_factory(User, Profile, form=ProfileForm, extra=1)
-
-# class ProfileDetailView(LoginRequiredMixin, UpdateView):
-#   model = User
-#   fields = ("username",)
-#   template_name = "user/profile.html"
-#   success_url = "."
-
-#   def get_context_data(self, **kwargs):
-#     context = super(ProfileDetailView, self).get_context_data(**kwargs)
-#     if self.request.POST:
-#       context['profile'] = ProfileFormSet(self.request.POST)
-#     else:
-#       context['profile'] = ProfileFormSet()
-#     return context
-  
-#   def form_valid(self, form):
-#     context = self.get_context_data()
-#     profile = context['profile']
-#     with transaction.atomic():
-#       self.object = form.save()
-
-#       if profile.is_valid():
-#         profile.instance = self.object
-#         profile.save()
-    
-#     return super(ProfileDetailView, self).form_valid(form)
-
-#   def get_object(self):
-#     return self.request.user
-
-
+ProfilePropicFormSet = forms.models.inlineformset_factory(
+    User, Profile, form=ProfileForm, can_delete=False,
+    extra=0, min_num=1, validate_min=True,
+)
 
 class ProfileDetailView(LoginRequiredMixin, UpdateView):
   model = User
@@ -66,20 +39,26 @@ class ProfileDetailView(LoginRequiredMixin, UpdateView):
   success_url = "."
 
   def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    if self.request.method == "POST":
-      context['profileform'] = ProfileForm(self.request.POST)
+    context = super(ProfileDetailView, self).get_context_data(**kwargs)
+    if self.request.POST:
+      context['profileform'] = ProfilePropicFormSet(
+        self.request.POST,
+        self.request.FILES,
+        instance=self.object
+      )
     else:
-      context['profileform'] = ProfileForm()
-    return context
+      context['profileform'] = ProfilePropicFormSet(instance=self.object)
+    return context | {'pagename': 'Profile'}
   
   def form_valid(self, form):
     context = self.get_context_data()
     profile = context.get('profileform')
     if profile.is_valid():
-      print(profile.cleaned_data['propic'])
-    #   profile.instance = self.object
-    #   profile.save()
+      self.object = form.save()
+      profile.instance = self.object
+      profile.save()
+    else:
+      return self.form_invalid(form)
   
     return super().form_valid(form)
 
