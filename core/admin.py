2fa184a75bc5dbcfb2a79eb34b16062cdc6fbc32
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-06-03 23:59:58 +0200

    Add Profile to Admin permissions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+  exclude = ["user"]  # Admin can't change this association
